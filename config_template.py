7927183a9b0ef7e99430baaf05281ba78ed200ad
JonathanZ <zez451@usask.ca>
2025-06-29 20:35:39 -0600

    Poilished .gitignore, and create new config template that others can use while keeping my own api key safe

---
+++ config_template.py
@@ -0,0 +1,6 @@
+# Google Places API Key And Configuration Template
+# Copy this file to config.py and add your actual API key
+
+GOOGLE_PLACES_API_KEY = "It's where bro's api key should stay"  # Replace with your actual Google API key
+MAX_RESULTS = 150
+RESULTS_FOLDER = "results"
